b81f11d5bf38bae0936a2607002d2c398658c1f4
wppoland <szatkowski.mariusz+github@gmail.com>
2026-07-28 09:30:07 +0200

    i18n: translate the PRO upgrade panel into Polish, German and Spanish

--- plogins-pickup.php
+++ plogins-pickup.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Pickup - Local Pickup for WooCommerce
  * Plugin URI:        https://plogins.com/plogins-pickup/
  * Description:        Let customers choose a pickup location and time slot at checkout.
- * Version:           1.0.3
+ * Version:           1.0.4
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Requires Plugins:  woocommerce
@@ -25,7 +25,7 @@ namespace Pickup;
 
 defined('ABSPATH') || exit;
 
-const VERSION     = '1.0.3';
+const VERSION     = '1.0.4';
 const PLUGIN_FILE = __FILE__;
 
 define('PICKUP_DIR', plugin_dir_path(__FILE__));
